ce8aa389cbbbc850271f128f997b06bdd2ac1ae6
Roman Štofa <stofa@axenta.cz>
2025-10-02 13:28:07 +0200

    Update safer option

--- cv3.cpp
+++ cv3.cpp
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-
 int main(){
-    int *rychlostZvuk;
-    int *cas;
+    int rychlostZvuk;
+    int cas;
 
-    *rychlostZvuk = 340;
+    printf("Zadajte cas za ktery je pocut hrom (s): ");
+    scanf("%d", &cas);
 
-    printf("Zadajte cas za ktery je pocut hrom: (s)");
-    scanf("%d", cas);
-
-    printf("Vydalenost je %d metrov", (*cas)*(*rychlostZvuk));
+    printf("Vydalenost je %d metrov", (cas)*(rychlostZvuk));
     return 0;
 }
--- cv3_p3.cpp
+++ cv3_p3.cpp
@@ -2,13 +2,15 @@
 
 
 int main(){
-    int x1;
-    int x2;
-    int y1;
-    int y2;
+    int x1, x2, y1, y2;
 
     printf("Zadajte 4 cisla (x1, x2, y1, y2):");
-    scanf("%d %d %d %d", &x1, &x2, &y1 , &y2);
+    scanf("%d", &x1);
+    scanf("%d", &x2);
+    scanf("%d", &y1);
+    scanf("%d", &y2);
+
 
     printf("Sucet je [%d;%d]: ", (x1 + x2), (y1 + y2));
+    return 0;
 }
